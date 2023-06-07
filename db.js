42337b2090a42e5933058ebac04bd3e51d179df4
lordnate <lordnate.moose@gmail.com>
2023-06-06 21:15:43 -0500

    initial commit

---
+++ db.js
@@ -0,0 +1,23 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri()
+    // database:'jobly', host:'localhost', port:5432, password:'Valvenis'
+  });
+}
+db.connect();
+
+module.exports = db;
